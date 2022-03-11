dbfd7d5fb5cfa621eee7fc1726e77dc703d0c80e
Arlindo Boa <arlindojosboa@gmail.com>
2022-03-11 09:11:47 +0300

    Update totalInflationController.ts
    Saving IPC Index

--- src/controller/totalInflationController.ts
+++ src/controller/totalInflationController.ts
@@ -1,25 +1,15 @@
 import { Request, Response } from 'express';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { resolve } from 'path';
 import xlsx from 'xlsx';
-import isRawNumber from '../validations/rawNumber';
-
-type Inflation = {
-  year: number
-  mensal: number[],
-  homologa: number[]
-}
 
 export default {
   async create(request: Request, response: Response) {
-    const { mensalRawNumber, homologaRawNumber } = request.body;
     const inflationFile = request.file;
 
     if(!inflationFile) {
       return response.status(400).json({error: 'You must provide inflation file'});
     }
-
-    await isRawNumber.rawNumber({mensalRawNumber, homologaRawNumber});
 
     const filename = inflationFile.filename; 
     const name = filename.split('-')[0];
@@ -38,43 +28,49 @@ export default {
       header: 1,
     });
 
-    fs.readFile(dest, 'utf8', (err, file) => {
-      if(err) return response.status(500).json(err);
-
-      const parsedFile = JSON.parse(file);
-      const total: Inflation[] = parsedFile.total;
-
-      const totalInflation = total.map((inflationOneYear) => {  
-
-        if(inflationOneYear.year === Number(year)) {
-          
-          return {
-            year: Number(year),
-
-            mensal: data[mensalRawNumber].filter((num: number | null) => {
-              return num !== null && num !== Number(year) && typeof num !== 'string'
-            }),
-  
-            homologa: data[homologaRawNumber].filter((num: null | number) => {
-              return num !== null && num !== Number(year) && typeof num !== 'string'
-            }),
-          }
-        } else {
-          return inflationOneYear
-        }
+    const ipcFilter = (IPCs: any) => (year: number) => {
+      const ipcS = IPCs.filter((num: number | null) => {
+        return num !== null && num !== Number(year) && typeof num !== 'string'
       })
 
-      const inflationData = {...parsedFile, total: totalInflation}  
+      return {
+        Jan: ipcS[0],
+        Fev: ipcS[1],
+        Mar: ipcS[2],
+        Abr: ipcS[3],
+        Mai: ipcS[4],
+        Jun: ipcS[5],
+        Jul: ipcS[6],
+        Ago: ipcS[7],
+        Set: ipcS[8],
+        Out: ipcS[9],
+        Nov: ipcS[10],
+        Dez: ipcS[11],
+      }
+    }
+
+    const totalIPC = {
+      2016: ipcFilter(data[1])(2016),
+      2017: ipcFilter(data[2])(2017),
+      2018: ipcFilter(data[3])(2018),
+      2019: ipcFilter(data[4])(2019),
+      2020: ipcFilter(data[5])(2020),
+      2021: ipcFilter(data[6])(2021),
+      2022: ipcFilter(data[7])(2022),
+    }
+
+    try {
+      const parsedFile = JSON.parse(await fs.readFile(dest, 'utf8'))
+      parsedFile.total = totalIPC
+        
+      await fs.writeFile(dest, JSON.stringify(parsedFile))
+
+      await fs.unlink(filePath)
+
+      response.status(200).json(totalIPC);
       
-      fs.writeFile(dest, JSON.stringify(inflationData), (err) => {
-        if(err) return response.status(500).json(err);
-      })
-
-      fs.unlink(filePath, (err) => {
-        if(err) return response.status(500).json(err);
-      })
-
-      response.status(200).json(inflationData);
-    })
+    } catch (err) {
+      response.status(500).json(err)
+    }
   }
 }
