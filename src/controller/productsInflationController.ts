ef5d250814f2e9785727a261a9bc9c04f9972757
Arlindo Boa <arlindojosboa@gmail.com>
2022-03-11 09:13:17 +0300

    Update productsInflationController.ts
    Extracting IPC

--- src/controller/productsInflationController.ts
+++ src/controller/productsInflationController.ts
@@ -1,28 +1,16 @@
 import { Request, Response } from 'express';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { resolve } from 'path';
 import xlsx from 'xlsx';
-import isRawNumber from '../validations/rawNumber';
 
-type ProductData = {
-  year: number
-  homologa: number[]
-}
-
-interface ProductInflation {
-  name: string
-  data: ProductData[]
-}
 export default {
   async create(request: Request, response: Response) {
-    const { dataArrayIndex, productArrayIndex, year16, year17, year18, year19, year20, year21 } = request.body;
+    const { } = request.body;
     const inflationFile = request.file;
 
     if(!inflationFile) {
       return response.status(400).json({error: 'You must provide inflation file'});
     }
-
-    // await isRawNumber.rawNumber({mensalRawNumber, homologaRawNumber});
 
     const filename = inflationFile.filename; 
     const name = filename.split('-')[0];
@@ -38,116 +26,142 @@ export default {
     const data: any = xlsx.utils.sheet_to_json(file.Sheets[firstTabName], {
       blankrows: false,
       header: 1,
-    })[Number(dataArrayIndex)]   
-    
-    // 2016
-    const count2016 = data.filter((d: number) => d === year16).length;    
-    
-    if(count2016 > 1) 
-    return response.status(400).json({error: `You have more than one of that index. ${year16}`});
-    
-    const startIndex2016 = data.indexOf(Number(year16));                       
-
-    const i2016 = data.splice(startIndex2016, 12);
-
-
-    // 2017
-    const count2017 = data.filter((d: number) => d === year17).length;    
-    
-    if(count2017 > 1) 
-    return response.status(400).json({error: `You have more than one of that index. ${year17}`});
-    
-    const startIndex2017 = data.indexOf(Number(year17));                       
-
-    const i2017 = data.splice(startIndex2017, 12);
-
-    // 2018
-    const count2018 = data.filter((d: number) => d === year18).length;      
-    
-    if(count2018 > 1) 
-    return response.status(400).json({error: `You have more than one of that index. ${year18}`});
-    
-    const startIndex2018 = data.indexOf(Number(year18));                         
-
-    const i2018 = data.splice(startIndex2018, 12);
-
-    // 2019
-    const count2019 = data.filter((d: number) => d === year19).length;    
-    
-    if(count2019 > 1) 
-    return response.status(400).json({error: `You have more than one of that index. ${year19}`});
-    
-    const startIndex2019 = data.indexOf(Number(year19));                  
-
-    const i2019 = data.splice(startIndex2019, 12);
-
-    // 2020
-    const count2020 = data.filter((d: number) => d === year20).length;  
-    
-    if(count2020 > 1) 
-    return response.status(400).json({error: `You have more than one of that index. ${year20}`});
-    
-    const startIndex2020 = data.indexOf(Number(year20));                       
-
-    const i2020 = data.splice(startIndex2020, 12);
-
-    // 2021
-    const count2021 = data.filter((d: number) => d === year21).length;   
-    
-    if(count2021 > 1)                                                          
-    return response.status(400).json({error: `You have more than one of that index. ${year21}`});
-    
-    const startIndex2021 = data.indexOf(Number(year21));                                
-
-    const i2021 = data.splice(startIndex2021, 12);                              
-
-    const productData = [
-      {
-        year: 2016,
-        homologa: i2016
-      },
-
-      {
-        year: 2017,
-        homologa: i2017
-      },
-      {
-        year: 2018,
-        homologa: i2018
-      },
-      {
-        year: 2019,
-        homologa: i2019
-      },
-      {
-        year: 2020,
-        homologa: i2020
-      },
-      {
-        year: 2021,
-        homologa: i2021
-      }
-    ]
-
-    fs.readFile(dest, 'utf8', (err, file) => {
-      if(err) return response.status(500).json(err);
-
-      const parsedFile = JSON.parse(file);
-      const products: ProductInflation[] = parsedFile.products;
-
-      products[Number(productArrayIndex)].data = productData                                     
-
-      const inflationData = {...parsedFile, products: products}      
-      
-      fs.writeFile(dest, JSON.stringify(inflationData), (err) => {
-        if(err) return response.status(500).json(err);
-      })
-
-      fs.unlink(filePath, (err) => {
-        if(err) return response.status(500).json(err);
-      })
-
-      response.status(200).json(productData);
     })
+
+    const ipcFilter = (IPCs: any) => {
+      const ipcS = IPCs.filter((num: number | null) => {
+        return num !== null && typeof num !== 'string'
+      })
+
+      return {
+        2016: {
+          Jan: ipcS[6],
+          Fev: ipcS[7],
+          Mar: ipcS[8],
+          Abr: ipcS[9],
+          Mai: ipcS[10],
+          Jun: ipcS[11],
+          Jul: ipcS[12],
+          Ago: ipcS[13],
+          Set: ipcS[14],
+          Out: ipcS[15],
+          Nov: ipcS[16],
+          Dez: ipcS[17],
+        },
+        2017: {
+          Jan: ipcS[18],
+          Fev: ipcS[19],
+          Mar: ipcS[20],
+          Abr: ipcS[21],
+          Mai: ipcS[22],
+          Jun: ipcS[23],
+          Jul: ipcS[24],
+          Ago: ipcS[25],
+          Set: ipcS[26],
+          Out: ipcS[27],
+          Nov: ipcS[28],
+          Dez: ipcS[29],
+        },
+        2018: {
+          Jan: ipcS[30],
+          Fev: ipcS[31],
+          Mar: ipcS[32],
+          Abr: ipcS[33],
+          Mai: ipcS[34],
+          Jun: ipcS[35],
+          Jul: ipcS[36],
+          Ago: ipcS[37],
+          Set: ipcS[38],
+          Out: ipcS[39],
+          Nov: ipcS[40],
+          Dez: ipcS[41],
+        },
+        2019: {
+          Jan: ipcS[42],
+          Fev: ipcS[43],
+          Mar: ipcS[44],
+          Abr: ipcS[45],
+          Mai: ipcS[46],
+          Jun: ipcS[47],
+          Jul: ipcS[48],
+          Ago: ipcS[49],
+          Set: ipcS[50],
+          Out: ipcS[51],
+          Nov: ipcS[52],
+          Dez: ipcS[53],
+        },
+        2020: {
+          Jan: ipcS[54],
+          Fev: ipcS[55],
+          Mar: ipcS[56],
+          Abr: ipcS[57],
+          Mai: ipcS[58],
+          Jun: ipcS[59],
+          Jul: ipcS[60],
+          Ago: ipcS[61],
+          Set: ipcS[62],
+          Out: ipcS[63],
+          Nov: ipcS[64],
+          Dez: ipcS[65],
+        },
+        2021: {
+          Jan: ipcS[66],
+          Fev: ipcS[67],
+          Mar: ipcS[68],
+          Abr: ipcS[69],
+          Mai: ipcS[70],
+          Jun: ipcS[71],
+          Jul: ipcS[72],
+          Ago: ipcS[73],
+          Set: ipcS[74],
+          Out: ipcS[75],
+          Nov: ipcS[76],
+          Dez: ipcS[77],
+        },
+        2022: {
+          Jan: ipcS[78],
+          Fev: ipcS[79],
+          Mar: ipcS[80],
+          Abr: ipcS[81],
+          Mai: ipcS[82],
+          Jun: ipcS[83],
+          Jul: ipcS[84],
+          Ago: ipcS[85],
+          Set: ipcS[86],
+          Out: ipcS[87],
+          Nov: ipcS[88],
+          Dez: ipcS[89],
+        }
+      }
+    }
+
+    const productIPC = {
+      'Alimentos e Bebidas não Alcoólicas': ipcFilter(data[2]),
+      'Bebidas Alcoólicas, Tabaco e Narcóticos': ipcFilter(data[5]),
+      'Vestuários e Calçados': ipcFilter(data[8]),
+      'Habitação, Água, Electricidade, Gás e Outros Combustíveis': ipcFilter(data[11]),
+      'Mobiliário, Artigos de Decoração, Equipamento Doméstico e Manutenção da Habitação': ipcFilter(data[16]),
+      'Saúde': ipcFilter(data[23]),
+      'Transportes': ipcFilter(data[26]),
+      'Comunicações': ipcFilter(data[30]),
+      'Lazer, Recreação e Cultura': ipcFilter(data[33]),
+      'Educação': ipcFilter(data[37]),
+      'Restaurantes, Hotéis, Cafés e Similares': ipcFilter(data[41]),
+      'Bens e Serviços Diversos': ipcFilter(data[43]),
+    }
+    
+    try {
+      const parsedFile = JSON.parse(await fs.readFile(dest, 'utf8'))
+      parsedFile.product = productIPC
+        
+      await fs.writeFile(dest, JSON.stringify(parsedFile))
+
+      await fs.unlink(filePath)
+
+      response.status(200).json(parsedFile);
+      
+    } catch (err) {
+      response.status(500).json(err)
+    }
   }
 }
